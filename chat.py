7bb2e6072dbc051cd17048c01f2d2a079f6c762b
Ipatest <35423613+Ipatest@users.noreply.github.com>
2018-01-17 00:04:29 +0300

    d

--- chat.py
+++ chat.py
@@ -13,7 +13,13 @@ def start(message):
     sent = bot.send_message(message.chat.id, 'Добрый день. Мы поможем Вам доставить груз из Китая. Для начала давайте познакомимся. Как Вас зовут?')
     bot.register_next_step_handler(sent, hello)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
 @server.route("/bot", methods=['POST'])
@@ -24,7 +30,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://chat3test.herokuapp.com/")
+    bot.set_webhook(url="https://chat3test.herokuapp.com/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
