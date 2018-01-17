083e3ff7313a5d88ac2af96c86bf8fcaadea849c
Ipatest <35423613+Ipatest@users.noreply.github.com>
2018-01-17 13:12:40 +0300

    a

--- chat.py
+++ chat.py
@@ -9,7 +9,13 @@ server = Flask(__name__)
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
 @bot.message_handler(commands=['start'])
